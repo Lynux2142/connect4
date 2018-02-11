984800e56226c3ab4cf4e48fbdfb189a4039ab8e
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-02-11 10:44:54 +0100

    ajout d'une fonction rand a la norme

--- srcs/test3.c
+++ srcs/test3.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 10:24:17 by lguiller          #+#    #+#             */
-/*   Updated: 2018/02/11 10:38:03 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/02/11 10:44:38 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,13 @@
 
 int		ft_rand(int min, int max)
 {
-	int		nb;
 	int		fd;
-	char	*c;
+	char	c[1];
 
-	if (!(c = ft_strnew(1)))
-		return (-1);
 	fd = open("/dev/random", O_RDONLY);
 	if (read(fd, c, 1) == -1)
 		return (-1);
-	nb = (int)(ft_abs(*c) % max + min);
-	return (nb);
+	return ((int)(ft_abs(c[0]) % max + min));
 }
 
 int		main(void)
