6e6dd4d8e1d2312b1864d1f126bdcdd0190e27b1
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-09-24 15:24:14 +0200

    ++

--- srcs/shape.c
+++ srcs/shape.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 13:15:39 by lguiller          #+#    #+#             */
-/*   Updated: 2018/09/21 12:56:27 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/09/24 14:30:41 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,34 +45,34 @@ void		ft_circle(t_img *img, t_circle circle)
 	int	y;
 	int	d;
 
-		x = 0;
-		y = circle.r;
-		d = circle.r - 1;
-		while (y >= x)
+	x = 0;
+	y = circle.r;
+	d = circle.r - 1;
+	while (y >= x)
+	{
+		ft_fill_pixel(img, circle.x + x, circle.y + y, circle.col);
+		ft_fill_pixel(img, circle.x + y, circle.y + x, circle.col);
+		ft_fill_pixel(img, circle.x - x, circle.y + y, circle.col);
+		ft_fill_pixel(img, circle.x - y, circle.y + x, circle.col);
+		ft_fill_pixel(img, circle.x + x, circle.y - y, circle.col);
+		ft_fill_pixel(img, circle.x + y, circle.y - x, circle.col);
+		ft_fill_pixel(img, circle.x - x, circle.y - y, circle.col);
+		ft_fill_pixel(img, circle.x - y, circle.y - x, circle.col);
+		if (d >= 2 * x)
 		{
-			ft_fill_pixel(img, circle.x + x, circle.y + y, circle.col);
-			ft_fill_pixel(img, circle.x + y, circle.y + x, circle.col);
-			ft_fill_pixel(img, circle.x - x, circle.y + y, circle.col);
-			ft_fill_pixel(img, circle.x - y, circle.y + x, circle.col);
-			ft_fill_pixel(img, circle.x + x, circle.y - y, circle.col);
-			ft_fill_pixel(img, circle.x + y, circle.y - x, circle.col);
-			ft_fill_pixel(img, circle.x - x, circle.y - y, circle.col);
-			ft_fill_pixel(img, circle.x - y, circle.y - x, circle.col);
-			if (d >= 2 * x)
-			{
-				d = d - 2 * x - 1;
-				++x;
-			}
-			else if (d < 2 * (circle.r - y))
-			{
-				d = d + 2 * y - 1;
-				--y;
-			}
-			else
-			{
-				d = d + 2 * (y - x - 1);
-				--y;
-				++x;
-			}
+			d = d - 2 * x - 1;
+			++x;
 		}
+		else if (d < 2 * (circle.r - y))
+		{
+			d = d + 2 * y - 1;
+			--y;
+		}
+		else
+		{
+			d = d + 2 * (y - x - 1);
+			--y;
+			++x;
+		}
+	}
 }
